f24ac88a4553ac9d1234df71a22c7dabd02db07f
Alexandr Andreev <cinic.rus@gmail.com>
2010-07-02 13:53:26 +0400

    bbbbb

--- js/init.js
+++ js/init.js
@@ -1,7 +1,7 @@
 document.documentElement.id = "js";
 (function(A){
 	A.fn.extend({
-		showForm:function(){
+		showCabinetForm:function(){
 			return this.each(function(){
 				var B=A(this);
 				var C=true;
@@ -28,5 +28,6 @@ $(function(){
 		$(this).carousel({loop:true,autoSlide:true,autoSlideInterval:6000,delayAutoSlide:6000,dispItems:1,effect:"fade",pagination:true});;
 	});
 	$('#header .cabinet').after('<fieldset class="cabinet-form"/>').siblings().load('cabinet.form.htm');
-	$('#header .cabinet').showForm();
+	$('#header .cabinet').showCabinetForm();
+	$('#header .nearest-branch-title').after('<div/>').siblings().load('branches.htm');
 });
