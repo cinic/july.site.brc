a65fdb2865e48280ab69adf70f16ca4bb31eccf9
Alexandr Andreev <cinic.rus@gmail.com>
2010-07-06 10:57:45 +0400

    Обновлен вывод языков и файл стилей

--- js/init.js
+++ js/init.js
@@ -1,52 +1,30 @@
 document.documentElement.id = "js";
 (function(A){
 	A.fn.extend({
-		showCabinetForm:function(){
+		showHide:function(elementClass, className){
 			return this.each(function(){
+				if(!elementClass || !className) {return }
 				var B=A(this);
 				var C=true;
-				A(".cabinet").bind("click focus",function(E){
+				A(elementClass.toString()).bind("click focus",function(E){
 					E.preventDefault();
 					if(!C){return }
 					C=false;
 					setTimeout(function(){C=true},500);
 					var D=A(this);
-					D.parent().toggleClass("cabinet-form-active");
+					D.parent().toggleClass(className.toString());
 				});
 				B.parent().mouseup(function(){return false});
 				A(document).mouseup(function(D){
-					if(A(D.target).parent("a.cabinet").length==0){
-						A(".cabinet").parent().removeClass("cabinet-form-active");
+					if(A(D.target).parent(elementClass.toString()).length==0){
+						A(elementClass.toString()).parent().removeClass(className.toString());
 					}
 				})
 			})
 		}
 	})
 })(jQuery);
-(function(B){
-	B.fn.extend({
-		showBranches:function(){
-			return this.each(function(){
-				var C=B(this);
-				var D=true;
-				B(".nearest-branch-title").bind("click focus",function(F){
-					F.preventDefault();
-					if(!D){return }
-					D=false;
-					setTimeout(function(){D=true},500);
-					var E=B(this);
-					E.parent().toggleClass("nearest-branch-active");
-				});
-				C.parent().mouseup(function(){return false});
-				B(document).mouseup(function(E){
-					if(B(E.target).parent(".nearest-branch-title").length==0){
-						B(".nearest-branch-title").parent().removeClass("nearest-branch-active");
-					}
-				})
-			})
-		}
-	})
-})(jQuery);
+
 (function(A){
 	A.fn.extend	({
 		showTabs:function(){
@@ -63,8 +41,8 @@ $(function(){
 	$('#slides #sections').load('slides.htm', function() {
 		$(this).carousel({loop:true,autoSlide:true,autoSlideInterval:6000,delayAutoSlide:6000,dispItems:1,effect:"fade",pagination:true});;
 	});
-	$('#header .cabinet').after('<fieldset class="cabinet-form"/>').siblings().load('cabinet.form.htm').showCabinetForm();
-	$('#header .nearest-branch-title').after('<div/>').siblings().load('branches.htm').showBranches();
-	
+	$('#header .cabinet').after('<fieldset class="cabinet-form"/>').siblings().load('cabinet.form.htm').showHide(".cabinet", "cabinet-form-active");
+	$('#header .nearest-branch-title').after('<div/>').siblings().load('branches.htm').showHide(".nearest-branch-title","nearest-branch-active");
+	$('#header #lang-select').after('<div/>').siblings().load('langs.htm').showHide("#active-lang", "active-lang-select");
 	$('.tabs dt').showTabs();
 });
